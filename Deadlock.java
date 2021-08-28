ebebb6981b63e401cf9c19efdbe31da24002ae0e
archiveOfMyLearning <89688304+archiveOfMyLearning@users.noreply.github.com>
2021-08-28 14:56:56 +0300

    Add files via upload

---
+++ Deadlock.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Deadlock {
+    public static void main(String[] args) {
+        Object outerLock = new Object();
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+        synchronized (outerLock){
+            Thread t1 = new Thread(()->{
+                synchronized (lock1){
+                    synchronized (outerLock){
+                        synchronized (lock2){}
+                    }
+                }
+            });
+            t1.start();
+            while (t1.getState()!= Thread.State.BLOCKED);
+            Thread t2 = new Thread(()->{
+                synchronized (lock2){
+                    synchronized (lock1){}
+                }
+            });
+            t2.start();
+            while (t2.getState()!= Thread.State.BLOCKED);
+        }
+        System.out.println("Deadlock obtained");
+    }
+}
